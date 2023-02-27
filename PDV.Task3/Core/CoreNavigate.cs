99c4d1d38a9c1f416f3de7f40a425794300c3f02
Даниил Пролыгин < @LAPTOP-MSQA7GO0>
2023-02-27 20:34:47 +0300

    Добавьте файлы проекта.

---
+++ PDV.Task3/Core/CoreNavigate.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows.Controls;
+
+namespace PDV.Task3.Core
+{
+    internal class CoreNavigate
+    {
+        public static Frame? MyConnection { get; set; }
+    }
+}
---
+++ PDV.Task3/View/Pages/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using PDV.Task3.Core;
+using PDV.Task3.View.TaskPages;
+using System.Windows;
+
+
+namespace PDV.Task3
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            CoreNavigate.MyConnection = MainFrame;
+            MainFrame.Navigate(new MainPage());
+        }
+    }
+}
